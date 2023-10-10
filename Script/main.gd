af37bbf211103926f8debbfccfdda5885e32f806
5P4RK3R <mahendransparker@gmail.com>
2023-10-10 15:30:43 +0530

    assets added

--- Script/main.gd
+++ Script/main.gd
@@ -3,8 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Utils.saveGame() 
-	Utils.loadGame()# Replace with function body.
+	pass
+#	Utils.saveGame() 
+#	Utils.loadGame()# Replace with function body.
  
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
